ef5f22f1356dc5b2b4c0a053a1ac26ef9eb9be05
JonuzNuhi <jnuhi690@gmail.com>
2023-07-29 15:21:59 +0200

    bug fixes + display list of recipes

---
+++ src/main/java/Springframework/repositories/UnitOfMeasureRepository.java
@@ -0,0 +1,14 @@
+package Springframework.repositories;
+
+import Springframework.domain.UnitOfMeasure;
+import org.springframework.data.repository.CrudRepository;
+
+import java.util.Optional;
+
+/**
+ * Created by jt on 6/13/17.
+ */
+public interface UnitOfMeasureRepository extends CrudRepository<UnitOfMeasure, Long> {
+
+    Optional<UnitOfMeasure> findByDescription(String description);
+}
---
+++ src/main/java/Springframework/services/RecipeService.java
@@ -0,0 +1,13 @@
+package Springframework.services;
+
+import Springframework.domain.Recipe;
+
+import java.util.Set;
+
+/**
+ * Created by jt on 6/13/17.
+ */
+public interface RecipeService {
+
+    Set<Recipe> getRecipes();
+}
